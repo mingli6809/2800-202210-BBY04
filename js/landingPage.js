2fc42e9ba319155d07fc5f8a91a0e4cb5bebe574
Sumit <veerkhanduri@gmail.com>
2022-05-04 21:11:24 -0700

    Made the menu for the desktop view and the hamburger menu for the mobile view

---
+++ js/landingPage.js
@@ -0,0 +1,12 @@
+let hamburgerMenu = document.getElementById("hamburgerMenu");
+let menu = document.getElementById("menu");
+
+function displayMenu() {
+    
+    menu.style.display = "grid";
+}
+
+function hideMenu() {
+menu.style.display = "none";
+}
+console.log("hello");

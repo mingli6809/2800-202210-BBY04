52f6854875a04bb72e1865de4cacb7c92f4d04b8
mingli <greenvideo6809@gmail.com>
2022-05-06 07:35:32 -0700

    database added

---
+++ database.sql
@@ -0,0 +1,12 @@
+CREATE DATABASE IF NOT EXISTS test2;
+use test2;
+CREATE TABLE IF NOT EXISTS customer (
+ID int NOT NULL AUTO_INCREMENT,
+email varchar(30),
+password varchar(30),
+code varchar(30),
+PRIMARY KEY (ID));
+INSERT INTO customer (email, password,code) values ("mingli6809@gmail.com", "333", "123");
+INSERT INTO customer (email, password,code) values ("melody@gmail.com", "333", null);
+INSERT INTO customer (email, password,code) values ("sylvie@gmail.com", "333", "123");
+INSERT INTO customer (email, password,code) values ("tom@gmail.com", "333", "456");

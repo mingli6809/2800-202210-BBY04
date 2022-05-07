4d23e0b9583b7717478ab9c1caa0edcb73653912
Eucarin <caffeinemousse@gmail.com>
2022-05-06 17:17:36 -0700

    Updated Phone screen landing page

--- js/landingPage.js
+++ js/landingPage.js
@@ -7,14 +7,13 @@ let menu = document.getElementById("menu");
 
 function displayMenu() {
     
-    menu.style.display = "grid";
-    hamburgerMenu.style.display = "none";
+menu.style.display = "grid";
+hamburgerMenu.style.display = "none";
 }
 
 function hideMenu() {
 menu.style.display = "none";
 hamburgerMenu.style.display = "flex";
-
 }
 
 document.getElementById("login").addEventListener("click", function (e) {

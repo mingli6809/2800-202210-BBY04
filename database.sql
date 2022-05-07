89cbd168345506f387c3b535f3cb692c60333420
mingli <greenvideo6809@gmail.com>
2022-05-06 17:04:17 -0700

    change database for email domain, limited to @my.bcit.ca

--- database.sql
+++ database.sql
@@ -6,7 +6,7 @@ email varchar(30),
 password varchar(30),
 code varchar(30),
 PRIMARY KEY (ID));
-INSERT INTO customer (email, password,code) values ("mingli6809@gmail.com", "333", "123");
-INSERT INTO customer (email, password,code) values ("melody@gmail.com", "333", null);
-INSERT INTO customer (email, password,code) values ("sylvie@gmail.com", "333", "123");
-INSERT INTO customer (email, password,code) values ("tom@gmail.com", "333", "456");
+INSERT INTO customer (email, password,code) values ("mingli6809@my.bcit.ca.com", "333", "123");
+INSERT INTO customer (email, password,code) values ("melody@my.bcit.ca.com", "333", null);
+INSERT INTO customer (email, password,code) values ("sylvie@my.bcit.ca.com", "333", "123");
+INSERT INTO customer (email, password,code) values ("tom@my.bcit.ca.com", "333", "456");

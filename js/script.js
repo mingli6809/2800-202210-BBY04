13f7a5a5bb293eff9031323114d9b100cee4b4e0
Sumit <veerkhanduri@gmail.com>
2022-05-19 22:13:56 -0700

    fixed the glitches in the nav

--- js/script.js
+++ js/script.js
@@ -23,61 +23,52 @@ GET("/nav", (response) => {
     hamburger.addEventListener("click", () => {
         hamburger.classList.toggle("active")
         navMenu.classList.toggle("active")
-        
-        
-        
-    })
+    });
 
     document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
         hamburger.classList.remove("active");
         navMenu.classList.remove("active");
-    }))
+    }));
     window.addEventListener("load", function(){
-        let landingPage = "http://localhost:8000/";
-        let loginPage = "http://localhost:8000/login_landing";
-        let signupPage = "http://localhost:8000/signup";
 
-        
-        if (window.location.href.contains("/login_landing")  || window.location.href.contains("/signup") ) {
+        if(this.window.location.href.indexOf("/login_landing") > -1 ||this.window.location.href.indexOf("/signup") > -1 ){
             document.querySelector(".logout").style.display = "none";
             document.querySelector(".profilePage").style.display = "none";
         }
     })
 
-    if (window.location.href == "http://localhost:8000/adminUsers") {
+    if (window.location.href.indexOf("/adminUsers") > - 1) {
         document.getElementById("header").style.position = "sticky";
         document.getElementById("header").style.top = "0";
 
     } else {}     
-})
+});
 
 GET("/footer", (res)=>{
     let doc = document.querySelector(".footer");
     doc.innerHTML = res;
-})
+});
 
 if (window.location.href == "http://localhost:8000/adminUsers") {
     document.getElementById("footer").style.position = "sticky";
 } else {
-}
+};
 
 let invert = false;
 let footer = document.querySelector(".footer");
 footer.addEventListener("click",function(){
     footer.addEventListener("click",function(){
         footer.addEventListener("click",function(){
-            if(document.body.classList.contains("invert")){
-                invert = false;
-            } else{
-                invert = true;
-                document.body.classList.add("invert");
-                document.body.classList.add("rotate");
+            if(document.body.classList.contains("easterEgg")){
+                document.body.classList.remove("easterEgg");
+                document.querySelector(".nav-menu").style.display = "flex";
+            } else {
+                document.body.classList.add("easterEgg");
+                document.querySelector(".nav-menu").style.display = "none";
+                setTimeout(function(){
+                    document.querySelector(".nav-menu").style.display = "flex";
+                },6000)
             }
         });
     });
 });
-
-window.addEventListener("load",function(){
-    if(invert){
-    document.body.classList.add("invert");
-}});

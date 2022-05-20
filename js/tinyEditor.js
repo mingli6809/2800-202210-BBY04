92e4d763c4b0e85b0cf1a3e2cd610fef2af16fdf
Eucarin <caffeinemousse@gmail.com>
2022-05-20 14:33:38 -0700

    updated css to accept tiny editor

--- js/tinyEditor.js
+++ js/tinyEditor.js
@@ -1,14 +1,14 @@
 tinymce.init({
     selector: '#description',
-    height: "50%",
+    height: "30%",
     width: "70%",
     margin: "20%",  
-    content_css : "../css/allEvent.css"
+    content_css : "./css/allEvent.css"
 });
 
 tinymce.init({
     selector: '#editDescription',
-    height: "50%",
+    height: "30%",
     width:"70%",  
     content_css : "./css/allEvent.css"
 });
